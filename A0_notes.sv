a3ad03021e50b4fcde81ed5096f7f43bca3c0c1e
Chethan <schethanchandu5@gmail.com>
2026-03-21 08:11:57 +0530

    Updated Section 3: Adapters

--- A0_notes.sv
+++ A0_notes.sv
@@ -117,3 +117,141 @@ class reg_block0 extends uvm_reg_block;
     lock_model(); // lock the model to prevent further modifications, this is optional but recommended to avoid accidental changes
   endfunction
 endclass
+
+
+
+********************Section 3: Understanding Adapters********************
+
+Converts register transaction to bus transaction and vice versa
+
+// Usage of Adapter
+
+// refer img_042.png
+reg sequence majorly have two methods:
+write(status, data);
+read(status, variable_to_store_data);
+
+//Typical flow
+1) call reg methods //read or write
+2) update struct //uvm_reg_bus_op struct will be updated with address, data, byte enable etc
+3) call adapter method to convert reg transaction to bus transaction //reg2bus or bus2reg method will be called based on the transaction type
+4) Update transaction //adapter will update the transaction with bus specific details like address, data, byte enable etc
+5) call driver method to send transaction to DUT
+6) Monitor will capture the bus transaction and call adapter method to convert bus transaction to reg transaction
+7) Update Mirror + Desired value //adapter will update the mirror and desired value based on the bus transaction
+
+
+//reg2bus
+regmodel.temp_reg_inst.write(status, 'h4); // call reg method to write data to register
+//address is not specified in the write method, it will be updated by the adapter based on the register address map
+
+//structure of uvm_reg_bus_op
+kind: UVM_WRITE(write)
+addr: address of the register from address map
+data: value to be written to the register //'h4 in this case
+status: status of the transaction, it will be updated by the reg method based on the success or failure of the transaction //OK, FAIL, etc
+
+
+reg2bus comverts reg transaction to bus transaction
+
+function uvm_sequence_item reg2bus(uvm_reg_bus_op reg_op);
+  // create bus transaction
+  transaction apb_bus_tr;
+  apb_bus_tr = transaction::type_id::create("apb_bus_tr");
+  
+  // update bus transaction with reg transaction details
+  apb_bus_tr.wr = (reg_op.kind == UVM_WRITE); // update transaction type (read/write) from reg transaction
+  apb_bus_tr.addr = reg_op.addr; // update address from reg transaction
+  if(apb_bus_tr.wr == 'b1) apb_bus_tr.din = reg_op.data; // update data from reg transaction
+  if(apb_bus_tr.wr == 'b1) apb_bus_tr.dout = reg_op.data; // update data from reg transaction
+  
+  return apb_bus_tr; // return the bus transaction
+endfunction
+
+
+//bus2reg
+function void bus2reg(uvm_seq_item bus_item, ref uvm_reg_bus_op reg_op);
+  transaction apb_bus_tr;
+
+  assert($cast(apb_bus_tr, bus_item)); // cast the bus item to bus transaction, this is optional but recommended to ensure that the bus item is of correct type
+
+  // update reg transaction with bus transaction details
+  reg_op.kind = bus_item.wr ? UVM_WRITE : UVM_READ; // update transaction type (read/write) from bus transaction
+  reg_op.data = bus_item.data; // update data from bus transaction
+  reg_op.addr = bus_item.addr; // update address from bus transaction
+  reg_op.status = UVM_IS_OK; // update status of the transaction, this is optional but recommended to indicate the success of the transaction
+endfunction
+
+
+
+
+//adapter code with native memory ports
+class top_adapter extends uvm_adapter;
+  `uvm_object_utils(top_adapter)
+
+  function new(string name = "top_adapter");
+    super.new(name);
+  endfunction
+
+  function uvm_sequence_item reg2bus(const ref uvm_reg_bus_op reg_op);
+    // create bus transaction
+    transaction apb_bus_tr;
+    apb_bus_tr = transaction::type_id::create("apb_bus_tr");
+    
+    // update bus transaction with reg transaction details
+    apb_bus_tr.wr = (reg_op.kind == UVM_WRITE); // update transaction type (read/write) from reg transaction
+    apb_bus_tr.addr = reg_op.addr; // update address from reg transaction
+    if(apb_bus_tr.wr == 'b1) apb_bus_tr.din = reg_op.data; // update data from reg transaction
+    if(apb_bus_tr.wr == 'b1) apb_bus_tr.dout = reg_op.data; // update data from reg transaction
+    
+    return apb_bus_tr; // return the bus transaction
+  endfunction
+
+  function void bus2reg(uvm_seq_item bus_item, ref uvm_reg_bus_op reg_op);
+    transaction apb_bus_tr;
+
+    assert($cast(apb_bus_tr, bus_item)); // cast the bus item to bus transaction, this is optional but recommended to ensure that the bus item is of correct type
+
+    // update reg transaction with bus transaction details
+    reg_op.kind = bus_item.wr ? UVM_WRITE : UVM_READ; // update transaction type (read/write) from bus transaction
+    reg_op.data = bus_item.data; // update data from bus transaction
+    reg_op.addr = bus_item.addr; // update address from bus transaction
+    reg_op.status = UVM_IS_OK; // update status of the transaction, this is optional but recommended to indicate the success of the transaction
+  endfunction
+endclass
+
+
+//adapter code with protocol specific ports
+//APB protocol specific adapter
+class apb_adapter extends uvm_adapter;
+  `uvm_object_utils(apb_adapter)
+
+  function new(string name = "apb_adapter");
+    super.new(name);
+  endfunction
+
+  virtual function uvm_sequence_item reg2bus( uvm_reg_bus_op reg_op);
+    // create bus transaction
+    apb_transaction apb_bus_tr;
+    apb_bus_tr = apb_transaction::type_id::create("apb_bus_tr");
+    
+    // update bus transaction with reg transaction details
+    apb_bus_tr.op = (reg_op.kind == UVM_WRITE) ? apb::WRITE : apb::READ; // update transaction type (read/write) from reg transaction
+    apb_bus_tr.addr = reg_op.addr; // update address from reg transaction
+    apb_bus_tr.data = reg_op.data; // update data from reg transaction    
+    return apb_bus_tr; // return the bus transaction
+  endfunction
+
+  virtualfunction void bus2reg(uvm_seq_item bus_item, uvm_reg_bus_op reg_op);
+    apb_transaction apb_bus_tr;
+
+    assert($cast(apb_bus_tr, bus_item)) else `uvm_fatal("APB_ADAPTER", "Failed to cast bus item to APB transaction"); // cast the bus item to bus transaction, this is optional but recommended to ensure that the bus item is of correct type
+
+    // update reg transaction with bus transaction details
+    reg_op.kind = (bus_item.op == apb::WRITE) ? UVM_WRITE : UVM_READ; // update transaction type (read/write) from bus transaction
+    reg_op.data = bus_item.data; // update data from bus transaction
+    reg_op.addr = bus_item.addr; // update address from bus transaction
+    reg_op.status = UVM_IS_OK; // update status of the transaction, this is optional but recommended to indicate the success of the transaction
+  endfunction
+endclass
+
